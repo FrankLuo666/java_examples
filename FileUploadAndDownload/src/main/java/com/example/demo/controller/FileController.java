5fb568fddd9e68671ad8763ed1eb88049d0df808
Frank Luo <FrankLuo666@gmail.com>
2021-09-12 01:16:05 +0900

    commit FileUploadAndDownload project

---
+++ FileUploadAndDownload/src/main/java/com/example/demo/controller/FileController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+*@Author: Frank Luo
+*@Date: 2021/8/31 16:36
+*/
+@Controller
+public class FileController {
+
+    @RequestMapping("/")
+    public String init(){
+        return "index";
+    }
+
+    @RequestMapping("/index")
+    public String initPage(){
+        return "index";
+    }
+}
